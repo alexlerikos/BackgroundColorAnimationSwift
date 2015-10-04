1a0c0b1e474f186e05f68b14a47478d1e3270ff6
alexlerikos <alerikos@gmail.com>
2015-10-04 13:22:03 -0700

    Initial Commit

---
+++ Background Animation in Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Background Animation in Swift
+//
+//  Created by alerikos on 10/4/15.
+//  Copyright © 2015 AlexLerikos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
