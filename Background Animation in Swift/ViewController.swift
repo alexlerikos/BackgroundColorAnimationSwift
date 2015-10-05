7d5685b08353594832976057eb6440ea72c78790
alexlerikos <alerikos@gmail.com>
2015-10-04 19:14:29 -0700

    background color animation function added

--- Background Animation in Swift/ViewController.swift
+++ Background Animation in Swift/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import ChameleonFramework
 
 class ViewController: UIViewController {
+	
+	// MARK: Properties
+	var backgroundColorIndex = 0
+	let backgroundColors:Array = [FlatRed,
+																FlatOrange,
+																FlatPink,
+																FlatPurple,
+																FlatYellow,
+																FlatGreen,
+																FlatBlue]
+
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		self.view.backgroundColor = backgroundColors[0]()
+		self.doBackgroundColorAnimation()
+		
+		
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	// MARK: Recursive Background Color Animation	
 
+	func doBackgroundColorAnimation() {
+		
+		
+		if (self.backgroundColorIndex >= self.backgroundColors.count){
+			self.backgroundColorIndex = 0
+		}
+		UIView.animateWithDuration(2.5,
+															delay: 0.0,
+															options: UIViewAnimationOptions.AllowUserInteraction,
+															animations: {() in
+																	self.view.backgroundColor = self.backgroundColors[self.backgroundColorIndex]()
+																	print("Animation i: ", self.backgroundColorIndex)
+															},
+															completion: {(value: Bool) in
+																	++self.backgroundColorIndex
+																	self.doBackgroundColorAnimation()
+															})
+	}
 
 }
 
